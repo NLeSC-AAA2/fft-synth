642c12ffdcd461a7c22fbc321269e534ee6313b0
Ben van Werkhoven <b.vanwerkhoven@esciencecenter.nl>
2020-01-24 15:24:56 +0100

    add commandline argument

--- app/Main.hs
+++ app/Main.hs
@@ -2,6 +2,7 @@
 
 module Main where
 
+import System.Environment
 import Data.Complex (Complex(..), realPart, imagPart)
 import qualified Data.Set as S
 import Data.Text (Text)
@@ -19,7 +20,10 @@ import TwiddleFactors
 main :: IO ()
 main = do
   T.IO.putStrLn genMacros
-  printFFT 20
+  args <- getArgs
+  case args of
+    [] -> putStrLn "Missing argument!"
+    (n:factors) -> printFFT (read n) (map read factors)
 
 generateTwiddle :: Text -> Shape -> IO Text
 generateTwiddle typeName shape = do
@@ -34,18 +38,22 @@ writeAlgorithm :: Algorithm -> IO Text
 writeAlgorithm Algorithm{..} = do
   twdefs <- T.unlines <$> mapM (generateTwiddle "float") (S.toList twiddles)
   codes  <- T.unlines <$> mapM (gen defaultArgs) (S.toList codelets)
-  return $ twdefs <> codes 
+  return $ twdefs <> codes
         <> "void fft(float const *input, float *output) {\n"
         <> T.unlines (map (("    " <>) . generate) statements)
         <> "}"
 
-printFFT :: Int -> IO ()
-printFFT n = do
-  let shape = [n, 1 :: Int]
-      a = Array "input" shape (fromShape shape 1) 0 :: Array (Complex Double)
-      b = Array "output" shape (fromShape shape 1) 0 :: Array (Complex Double)
-      alg' = fullFactorFFT n a b
+printFFT :: Int -> [Int] -> IO ()
+printFFT n factors = do
   case alg' of
     Left err -> T.IO.putStrLn $ "Error: " <> err
     Right alg -> T.IO.putStrLn =<< writeAlgorithm alg
-
+  where
+    shape = [n, 1 :: Int]
+    a = Array "input" shape (fromShape shape 1) 0 :: Array (Complex Double)
+    b = Array "output" shape (fromShape shape 1) 0 :: Array (Complex Double)
+    --alg' = fullFactorFFT n a b
+    --alg' = unHandle $ nFactorFFT [32, 32] a b
+    alg' = case factors of
+                [] -> fullFactorFFT n a b
+                (_:_) -> unHandle $ nFactorFFT factors a b
